40fe67876e7dd9d0f3cd3b5db5cf4176c90dd2b4
Robert Frank <robert.frank@manchester.ac.uk>
2013-08-30 11:17:14 +0100

    fixed queue modification and deployment bugs in torque_config_diff added confine statement to facts to prevent them from executing on non-torque nodes

--- lib/facter/torque_server_config.rb
+++ lib/facter/torque_server_config.rb
@@ -1,6 +1,7 @@
 # torque_server_config
 Facter.add(:torque_server_config) do
   confine :osfamily => 'RedHat'
+  confine :batchsystem => 'torque'
   setcode do
     tsc = Facter::Util::Resolution::exec("qmgr -c 'print server' | sed -n 's/^set server //p'")
     tsc.nil? ? nil : tsc.tr("\n", ',')
